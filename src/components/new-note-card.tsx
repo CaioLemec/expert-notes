c33ed9d9c0ad874fe98b7bfd085b2e776a694e01
Caio Lemec <caiolemec@gmail.com>
2024-02-08 17:15:28 -0300

    feat: add new modal component
    The creation of modals is necessary for the creation and editing of notes, which is why this element was developed.
    
    In addition, some libraries have been added to the project to streamline the application development, such as:
    
    Radix-ui -> Facilitating the development of modals.
    Sonner -> Adding toasts for user notifications.
    Date-fns -> Time formatting.
    Lucide-react -> Icons.

--- src/components/new-note-card.tsx
+++ src/components/new-note-card.tsx
@@ -1,13 +1,77 @@
+import * as Dialog from "@radix-ui/react-dialog";
+import { X } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { toast } from "sonner";
+
 export function NewNoteCard() {
+  const [shouldShowOnboarding, setShouldShowOnboarding] = useState<boolean>(true);
+  const [content, setContent] = useState<string>('');
+
+  function handleStartEditor() {
+    setShouldShowOnboarding(false);
+  }
+
+  function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+    setContent(event.target.value)
+    if (event.target.value === "") {
+      setShouldShowOnboarding(true);
+    }
+  }
+
+  function handleSaveNote(event: FormEvent<HTMLButtonElement>) {
+    event.preventDefault()
+    toast.success('Note created successfully.')
+  }
+
   return (
-    <div className="rounded-md bg-slate-700 p-5 space-y-3 overflow-hidden">
-      <span className="text-sm font-medium text-slate-200">Add Note</span>
-      <p className="text-sm leading-6 text-slate-200">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi
-        blanditiis voluptatem, alias est nobis accusantium aperiam repudiandae
-        maxime ex assumenda! Reiciendis eum quisquam tenetur debitis maiores
-        similique, voluptatibus cumque inventore.
-      </p>
-    </div>
+    <Dialog.Root>
+      <Dialog.Trigger className="rounded-md flex flex-col bg-slate-700 p-5 gap-3 text-left overflow-hidden hover:ring-2 hover:ring-slate-600 outline-none relative focus-visible:ring-2 focus-visible:ring-lime-400   ">
+        <span className="text-sm font-medium text-slate-200">Add note</span>
+        <p className="text-sm leading-6 text-slate-300">Start by recoring or typing a note</p>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="inset-0 fixed bg-black/50" />
+        <Dialog.Content className="overflow-hidden fixed left-1/2 top-1/2 h-[60vh] -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+          <form onSubmit={handleSaveNote} className="flex-1 flex flex-col">
+            <div className="flex flex-1 flex-col gap-3 p-5">
+              <span className="text-sm font-medium text-slate-200">
+                Add note
+              </span>
+              {shouldShowOnboarding ? (
+                <p className="text-sm leading-6 text-slate-300">
+                  Start by{" "}
+                  <button className="font-medium text-lime-400 hover:underline">
+                    recording
+                  </button>{" "}
+                  or{" "}
+                  <button
+                    onClick={handleStartEditor}
+                    className="font-medium text-lime-400 hover:underline"
+                  >
+                    typing
+                  </button>{" "}
+                  a note.
+                </p>
+              ) : (
+                <textarea
+                  autoFocus
+                  className="text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none"
+                  onChange={handleContentChanged}
+                />
+              )}
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-lime-400 py-4 text-center text-sm text-lime-950 outline-none font-medium hover:bg-lime-500"
+            >
+              Save note
+            </button>
+          </form>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 }
